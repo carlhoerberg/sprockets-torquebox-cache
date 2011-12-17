4a62f0eb3e0fb936f0b05f3e1ffe837aad44efd9
Carl Hörberg <carl.hoerberg@gmail.com>
2011-12-17 14:19:47 +0100

    Base64 encode the marshaled objects

--- lib/sprockets-torquebox-cache.rb
+++ lib/sprockets-torquebox-cache.rb
@@ -1,3 +1,4 @@
+require 'base64'
 module Sprockets
   module Cache
     # Utilites Torquebox Infinispan Cache for Sprocket caching
@@ -5,7 +6,7 @@ module Sprockets
     #     environment.cache = Torquebox::Infinispan::Cache.new(...)
     #
     class Torquebox
-      VERSION = "0.0.2"
+      VERSION = "0.0.3"
 
       def initialize(cache)
         @cache = cache
@@ -14,12 +15,12 @@ module Sprockets
       # Lookup value in cache
       def [](key)
         data = @cache.get key
-        Marshal.load data if data
+        Marshal.load Base64.decode64(data) if data
       end
 
       # Save value to cache
       def []=(key, value)
-        @cache.put key, Marshal.dump(value)
+        @cache.put key, Base64.encode64(Marshal.dump(value))
         value
       end
     end
